2ba5e7465731d0e4214bc9e3cb1b455d4415459a
eposha <freedoctime@gmail.com>
2019-10-30 18:09:06 +0300

    4

---
+++ JavaScript/lesson2/task3/index.js
@@ -0,0 +1,17 @@
+let goodNumber = 17,
+    anotherNumber = goodNumber,
+    anotherNumber = 77;
+
+console.log(goodNumber),
+    console.log(anotherNumber);
+
+let customer = {
+        name: 'John',
+        age: 17
+    },
+    anotherCustomer = customer;
+
+customer.age = 20;
+
+console.log(customer.age),
+    console.log(anotherCustomer.age);
