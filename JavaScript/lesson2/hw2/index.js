459c9e45c0d66a93d0863055152427812d0a5923
eposha <freedoctime@gmail.com>
2019-10-30 18:55:56 +0300

    5

--- JavaScript/lesson2/hw2/index.js
+++ JavaScript/lesson2/hw2/index.js
@@ -3,7 +3,7 @@ let a = 4,
 
 console.log('a: ', a),
     console.log('b: ', b),
-    console.log('a - b: ', a - b),
-    console.log('a + b: ', a + b),
-    console.log('a / b: ', a / b),
-    console.log('a * b: ', a * b);
+    console.log('a - b ', '= ', a - b),
+    console.log('a + b ', '= ', a + b),
+    console.log('a / b ', '= ', a / b),
+    console.log('a * b ', '= ', a * b);
