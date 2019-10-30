246636e71980b66fbce80774f229ebb6af1ebf09
eposha <freedoctime@gmail.com>
2019-10-30 17:50:32 +0300

    4

---
+++ JavaScript/lesson3/index.js
@@ -0,0 +1,28 @@
+const a = 17;
+const b = 17.17;
+const c = '17';
+const d = "17";
+const e = "I'm a string";
+const f = false;
+const g = true;
+const h = null;
+const i = undefined;
+const j = Symbol("I'm a Symbol");
+const k = { name: 'John' };
+const l = [1, 2, 3, 4];
+const m = function run() {
+    return 17;
+};
+console.log('a: ', typeof a);
+console.log('a: ', typeof b);
+console.log('a: ', typeof c);
+console.log('a: ', typeof d);
+console.log('a: ', typeof e);
+console.log('a: ', typeof f);
+console.log('a: ', typeof g);
+console.log('a: ', typeof h);
+console.log('a: ', typeof i);
+console.log('a: ', typeof j);
+console.log('a: ', typeof k);
+console.log('a: ', typeof l);
+console.log('a: ', typeof m);
