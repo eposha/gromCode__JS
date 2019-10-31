54b40d6fde736dbf4179f4c1b5529955865126aa
eposha <freedoctime@gmail.com>
2019-10-31 12:52:14 +0300

    4

--- JavaScript/lesson3/task2__explicit__casting/index.js
+++ JavaScript/lesson3/task2__explicit__casting/index.js
@@ -10,16 +10,16 @@ console.log("'17' to number is " + +'17'),
     console.log("false to number is " + +false),
     console.log("null to number is " + +null),
     console.log("undefined to number is " + +undefined),
-    console.log("'  20  '  to number is " + +'  20  '),
-    console.log("' ' to number is " + +' '),
-    console.log("'  30d  ' to number is " + +'  30d  ');
+    console.log("'   20   ' to number is " + Number('   20   ')),
+    console.log("'      ' to number is " + Number('      ')),
+    console.log("'   30d   ' to number is " + Number('   30d   '));
 
 console.log('null to boolean is ' + Boolean(null)),
     console.log('undefined to boolean is ' + Boolean(undefined)),
     console.log('0 to boolean is ' + Boolean(0)),
     console.log('-0 to boolean is ' + Boolean(-0)),
     console.log('NaN to boolean is ' + Boolean(NaN)),
-    console.log("'   20   ' to number is " + Number('   20   ')),
-    console.log("'      ' to number is " + Number('      ')),
-    console.log("'   30d   ' to number is " + Number('   30d   ')),
+    console.log(`'' to boolean is ` + Boolean('')),
+    console.log(`' ' to boolean is ` + Boolean(' ')),
+    console.log('17 to boolean is ' + Boolean(17)),
     console.log("'Hello' to boolean is " + Boolean('Hello'));
