ca9a0803ade88b4bca090bdfe94b615aaeae6ae8
eposha <freedoctime@gmail.com>
2019-10-31 15:32:45 +0300

    5

--- JavaScript/lesson3/task4__logical-operators/index.js
+++ JavaScript/lesson3/task4__logical-operators/index.js
@@ -26,9 +26,14 @@ console.log("true && true && true: ", true && true && true);
 true || 'text';
 undefined || null || 0 || '' || null;
 
+console.log("'text' || false: ", 'text' || false);
+console.log("true || 'text': ", true || 'text');
+console.log("undefined || null || 0 || '' || null: ", undefined || null || 0 || '' || null);
+
 const customAmount = null; // не выводим
 const defaultAmount = 17; // не выводим
 const amount = customAmount || defaultAmount; // выводим значкние переменной amount в формате console.log('amount: ', amount);
+console.log('amount: ', amount);
 
 
 /* && находит первое ложное значение */
@@ -36,9 +41,18 @@ const amount = customAmount || defaultAmount; // выводим значкние
 true && 'text';
 undefined && null && 0 && '' && null;
 
+console.log("'text' || false: ", 'text' || false);
+console.log("'true && 'text': ", true && 'text');
+console.log("'undefined && null && 0 && '' && null: ", undefined && null && 0 && '' && null);
+
 
 /* оператор отрицания (OR) */
 !true;
 !false;
 !'text';
 !!'text';
+
+console.log('!true :', !true);
+console.log('!false :', !false);
+console.log(`!'text' :`, !'text');
+console.log(`!!'text' :`, !!'text');
