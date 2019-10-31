7e53599c44daf1a01f5638dda462da42bb6cb78c
eposha <freedoctime@gmail.com>
2019-11-01 00:50:35 +0300

    5

--- JavaScript/lesson4/hw2/index.js
+++ JavaScript/lesson4/hw2/index.js
@@ -3,7 +3,7 @@ const n = 5;
 let result = 0;
 
 
-for (let i = 0; i <= n; i++) {
+for (let i = m; i <= n; i++) {
     if ((i % 2 == 0) && (i % 4 != 0)) {
         result += i;
     } else if (i % 3 == 0) {
