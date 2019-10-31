8a950d4b1ed02c3dc3c9b9336e021551022d2d59
eposha <freedoctime@gmail.com>
2019-10-31 23:28:40 +0300

    5

--- JavaScript/lesson4/hw3/index.js
+++ JavaScript/lesson4/hw3/index.js
@@ -1,15 +1,14 @@
-let j = 0;
+let sum = 0;
 
 for (let i = 0; i <= 1000; i++) {
     if (i % 2 != 0) {
         console.log('Found');
-        j += i;
-        if (j * 5 > 5000) {
+        ++sum;
+        if (sum * 5 > 5000) {
             console.log('Bigger');
-        } else if (j * 5 == 5000) {
-            console.log('equal');
         } else {
-            console.log('Smaller');
+            console.log('Smaller or equal');
         }
     }
 }
+// console.log(sum);
