bb2ed12c40557a033b16f0d49aa7bd6adf58f42f
eposha <freedoctime@gmail.com>
2019-10-31 11:15:35 +0300

    5

--- JavaScript/lesson3/task2__explicit__casting/index.js
+++ JavaScript/lesson3/task2__explicit__casting/index.js
@@ -5,12 +5,11 @@ console.log('17 to string is ' + '17'),
     console.log('undefined to string is ' + 'undefined'),
     console.log('0 to string is ' + '0');
 
-
-console.log("17 to number is " + +'17'),
-    console.log("true to number is " + +'true'),
-    console.log("false to number is " + +'false'),
-    console.log("null to number is " + +'null'),
-    console.log("undefined' to number is " + +'undefined'),
-    console.log("20 to number is " + +'20'),
-    console.log("'' to number is " + +''),
-    console.log("30d to number is " + +'30d');
+console.log("' 17 ' to number is " + +' 17 '),
+    console.log("' true ' to number is " + +' true '),
+    console.log("' false ' to number is " + +' false '),
+    console.log("' null ' to number is " + +' null '),
+    console.log("' undefined ' to number is " + +' undefined '),
+    console.log("' 20 ' to number is " + +' 20 '),
+    console.log("' ' to number is " + +' '),
+    console.log("' 30d ' to number is " + +' 30d ');
