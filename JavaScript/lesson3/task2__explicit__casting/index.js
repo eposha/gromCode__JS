2f4c43fa43deaf90497cc299dd93b8bf4ace7bc3
eposha <freedoctime@gmail.com>
2019-10-31 10:55:10 +0300

    4

--- JavaScript/lesson3/task2__explicit__casting/index.js
+++ JavaScript/lesson3/task2__explicit__casting/index.js
@@ -1,5 +1,5 @@
 console.log('17 to string is ' + '17'),
-    console.log('-17.17 to string is ' + '- 17.17'),
+    console.log('-17.17 to string is ' + '-17.17'),
     console.log('false to string is ' + 'false'),
     console.log('null to string is ' + 'null'),
     console.log('undefined to string is ' + 'undefined'),
