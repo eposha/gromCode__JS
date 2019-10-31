8adbad6e0aec88dd8da10f671ca1632ede8c6705
eposha <freedoctime@gmail.com>
2019-10-31 11:54:53 +0300

    5

--- JavaScript/lesson3/task3__comparison-operators/index.js
+++ JavaScript/lesson3/task3__comparison-operators/index.js
@@ -36,6 +36,7 @@ false === 0
 console.log("17 === 1: ", 17 === 1),
     console.log("17 === 17: ", 17 === 17),
     console.log("17 === '17': ", 17 === '17'),
+    console.log("17 === true: ", 17 === true),
     console.log("'0' === '': ", '0' === ''),
     console.log("true === false: ", true === false),
     console.log("true === true: ", true === true),
