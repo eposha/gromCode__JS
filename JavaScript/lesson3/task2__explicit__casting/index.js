21dc7091c561bfca5c748750f2e78186588ad944
eposha <freedoctime@gmail.com>
2019-10-31 12:33:28 +0300

    5

--- JavaScript/lesson3/task2__explicit__casting/index.js
+++ JavaScript/lesson3/task2__explicit__casting/index.js
@@ -10,9 +10,9 @@ console.log("'17' to number is " + +'17'),
     console.log("false to number is " + +false),
     console.log("null to number is " + +null),
     console.log("undefined to number is " + +undefined),
-    console.log("' 20 '  to number is " + +' 20 '),
+    console.log(`' 20 '  to number is ` + +' 20 '),
     console.log("' ' to number is " + +' '),
-    console.log("'30d' to number is " + +'30d');
+    console.log(`'30d' to number is ` + +' 30d ');
 
 console.log('null to boolean is ' + Boolean(null)),
     console.log('undefined to boolean is ' + Boolean(undefined)),
