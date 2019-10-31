010762cf8dc9fce76e9e9743ff1c26522aa70418
eposha <freedoctime@gmail.com>
2019-10-31 11:38:23 +0300

    5

---
+++ JavaScript/lesson3/task3__comparison-operators/index.js
@@ -0,0 +1,49 @@
+/* сравнение чисел */
+
+const a = 17;
+const b = 5;
+a > b;
+a <= b;
+a == b;
+a != b;
+
+console.log("'a' < 'b': ", 'a' > 'b'),
+    console.log("'a' <= 'b': ", 'a' <= 'b'),
+    console.log("'a'== 'b': ", 'a' == 'b'),
+    console.log("'a' != 'b': ", 'a' != 'b');
+
+
+/* сравнение строк */
+
+'a' < 'b';
+'ab' > 'a';
+
+/* строгое сравнение */
+
+17 === 1;
+17 === 17;
+17 === '17';
+17 === true;
+'0' === '';
+true === false;
+true === true;
+null === undefined;
+false === 0
+
+
+/* нестрогое сравнение */
+
+17 == '17';
+'0' == '';
+0 == '';
+null == undefined;
+false == 0;
+
+
+/* осторожно c null и undefined */
+
+undefined == null;
+undefined == 0;
+null == 0;
+undefined < 0;
+undefined > 0;
