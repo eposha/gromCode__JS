110bb74162f1e3b2771d26fd185003baec190f7e
eposha <freedoctime@gmail.com>
2019-11-01 10:36:51 +0300

    5

--- JavaScript/lesson4/hw2/index.js
+++ JavaScript/lesson4/hw2/index.js
@@ -1,20 +1,16 @@
 const m = 1;
-const n = 50;
+const n = 18;
 let result = 0;
-let b = m;
-let c = n;
 
-while (b <= c) {
-    if ((c - b % 2 == 0) && (c - b % 4 != 0)) {
-        result += c - b;
-    } else if (c - b == 0) {
-        result -= c - b;
-    } else if (c - b % 4 == 0) {
-        result *= c - b;
-    } else if (c - b % 5 === 0) {
-        console.log(c - b);
+for (let i = m; i <= n; i++) {
+    if (i % 2 === 0 && i % 4 !== 0) {
+        result += i;
+    } else if (i % 3 === 0) {
+        result -= i;
+    } else if (i % 4 === 0) {
+        result *= i;
     }
-    c--
+    if (i % 5 === 0) {
+        console.log(i);
+    }
 }
-
-console.log(result);
