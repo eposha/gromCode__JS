614f1ef65c1aa17654a19ee6a3b2d435f319a60f
eposha <freedoctime@gmail.com>
2019-11-02 19:07:38 +0300

    5

--- JavaScript/lesson6__array/hw3/index.js
+++ JavaScript/lesson6__array/hw3/index.js
@@ -1,7 +1,13 @@
 function sortAsc(array) {
-    let sortUPArr = [];
 
-    for (let i = 0; i < array.length; i++) {
-
+    for (let i = 0; i < array.length - 1; i++) {
+        for (let j = 0; j < array.length - 1 - i; j++) {
+            if (array[j + 1] < array[j]) {
+                let max = array[j + 1];
+                array[j + 1] = array[j];
+                array[j] = max;
+            }
+        }
     }
+    return array;
 }
