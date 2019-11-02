7dbc259202ad8b5bc69f839fed43da947154324b
eposha <freedoctime@gmail.com>
2019-11-02 12:20:23 +0300

    4

--- JavaScript/lesson6__array/hw8/index.js
+++ JavaScript/lesson6__array/hw8/index.js
@@ -1,11 +1,5 @@
-const numbers = [5, 6, 55, 78, 30, 85];
-
 function cloneArr(arr) {
     const [...newArr] = arr;
 
-    newArr[2] = 'Tom';
-    console.log(arr);
-    console.log(newArr);
+    return newArr;
 }
-
-console.log(cloneArr(numbers));
