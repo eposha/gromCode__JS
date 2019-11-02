11de70244f0fc6d5495323146ef7a5331a43213a
eposha <freedoctime@gmail.com>
2019-11-02 14:39:45 +0300

    5

--- JavaScript/lesson6__array/hw4/index.js
+++ JavaScript/lesson6__array/hw4/index.js
@@ -1,11 +1,17 @@
-function withdraw(clients, balances, client, amount) {
-    for (let i = 0; i < clients.length; i++) {
-        if (clients[i] == client) {
-            if (balances[i] - amount >= 0) {
-                return balances[i] - amount;
-            } else {
-                return -1;
+function uniqueCount(array) {
+    let quantityUniqueCount = array.length;
+    nextStep: for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array.length; j++) {
+            if (array[i] == array[j]) {
+                for (let c = j + 1; c < array.length; c++) {
+                    if (array[i] == array[c]) {
+                        quantityUniqueCount--;
+                        continue nextStep;
+                    }
+                }
             }
         }
+
     }
+    return quantityUniqueCount;
 }
