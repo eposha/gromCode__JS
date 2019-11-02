ffc6d106ad4ad689b3b812fd2ff91c300894a1ca
eposha <freedoctime@gmail.com>
2019-11-02 19:10:14 +0300

    5

--- JavaScript/lesson6__array/hw3/index.js
+++ JavaScript/lesson6__array/hw3/index.js
@@ -11,3 +11,19 @@ function sortAsc(array) {
     }
     return array;
 }
+
+
+
+function sortDesc(array) {
+
+    for (let i = 0; i < array.length - 1; i++) {
+        for (let j = 0; j < array.length - 1 - i; j++) {
+            if (array[j + 1] > array[j]) {
+                let max = array[j + 1];
+                array[j + 1] = array[j];
+                array[j] = max;
+            }
+        }
+    }
+    return array;
+}
