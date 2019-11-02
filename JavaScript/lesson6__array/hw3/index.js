5ce0e822b213190fefcd876f8d3c17b3a52eecda
eposha <freedoctime@gmail.com>
2019-11-02 11:33:11 +0300

    5

--- JavaScript/lesson6__array/hw3/index.js
+++ JavaScript/lesson6__array/hw3/index.js
@@ -1,11 +1,7 @@
-function getSum(a, b) {
-    let sum = 0;
-    for (let i = a; i <= b; i++) {
-        if (i % 2 == 0) {
-            sum += i;
-        }
-    }
-    return sum;
-}
+function sortAsc(array) {
+    let sortUPArr = [];
 
-// console.log(getSum(5, 25));
+    for (let i = 0; i < array.length; i++) {
+
+    }
+}
