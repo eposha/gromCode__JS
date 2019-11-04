a26c1df27a19b4a47abc3894a5f82b360c291477
eposha <freedoctime@gmail.com>
2019-11-04 23:26:50 +0300

    4

--- JavaScript/lesson9__object/task2/index.js
+++ JavaScript/lesson9__object/task2/index.js
@@ -1,11 +1,13 @@
-const copyObj = obj => {
-    const copyArray = Object.assign({}, obj);
-    return copyArray;
-}
+// const copyObj = obj => {
+//     const copyArray = Object.assign({}, obj);
+//     return copyArray;
+// }
+
+const copyObj = obj => Object.assign({}, obj);
 
 
-// console.log(copyObj({
-//     'Tom': 18,
-//     'Bob': 24,
-//     'Ivan': 17.
-// }));
+console.log(copyObj({
+    'Tom': 18,
+    'Bob': 24,
+    'Ivan': 17.
+}));
