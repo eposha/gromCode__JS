39e55e353303209c32ee665a28dffd707dbc64c3
eposha <freedoctime@gmail.com>
2019-11-05 00:37:42 +0300

    5

--- JavaScript/lesson9__object/hw1/index.js
+++ JavaScript/lesson9__object/hw1/index.js
@@ -1,7 +1,7 @@
 const getCustomersList = obj => {
 
-    let arrWithObj = Object.values(obj);
-    let objWithKey = Object.keys(obj);
+    const arrWithObj = Object.values(obj);
+    const objWithKey = Object.keys(obj);
 
 
     for (let i = 0; i < arrWithObj.length; i++) {
@@ -14,26 +14,26 @@ const getCustomersList = obj => {
 
 
 
-// const customers = {
-//     'customer-id-1': {
-//         name: 'William',
-//         age: 54
-//     },
-//     'customer-id-2': {
-//         name: 'Tom',
-//         age: 17
-//     },
-//     'customer-id-3': {
-//         name: 'Inna',
-//         age: 22
-//     },
-//     'customer-id-4': {
-//         name: 'Ivan',
-//         age: 18
-//     },
-// };
+const customers = {
+    'customer-id-1': {
+        name: 'William',
+        age: 54
+    },
+    'customer-id-2': {
+        name: 'Tom',
+        age: 17
+    },
+    'customer-id-3': {
+        name: 'Inna',
+        age: 22
+    },
+    'customer-id-4': {
+        name: 'Ivan',
+        age: 18
+    },
+};
 
-// console.log(getCustomersList(customers));
+console.log(getCustomersList(customers));
 
 // const obj = {
 //     'Tom': 18,
