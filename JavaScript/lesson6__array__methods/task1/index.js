bf3e1cbae369316abf9f2595d1d20ba11f7a2d5c
eposha <freedoctime@gmail.com>
2019-11-04 11:08:14 +0300

    5

--- JavaScript/lesson6__array__methods/task1/index.js
+++ JavaScript/lesson6__array__methods/task1/index.js
@@ -1,10 +1,18 @@
 const getSpecialNumbers = numbers => {
     let specialNumbers = [];
 
-    for (let i = 0; i < numbers.length; i++) {
-        if (numbers[i] % 3 == 0) {
-            specialNumbers.push(numbers[i]);
+    function checkIfSpecialNumber(num) {
+        if (num % 3 == 0) {
+            specialNumbers.push(num);
         }
     }
+
+    numbers.forEach(checkIfSpecialNumber);
+
+    // for (let i = 0; i < numbers.length; i++) {
+    //     if (numbers[i] % 3 == 0) {
+    //         specialNumbers.push(numbers[i]);
+    //     }
+    // }
     return specialNumbers;
 };
--- JavaScript/lesson6__array__methods/task2/index.js
+++ JavaScript/lesson6__array__methods/task2/index.js
@@ -1,13 +1,1 @@
-function getSum(arr) {
-    let sum = 0;
-
-    if (!Array.isArray(arr)) {
-        return null;
-    }
-
-    for (let i = 0; i < arr.length; i++) {
-        sum += arr[i];
-    }
-
-    return sum;
-}
+const
