33dcc0201897668dbe3871157147c3c36746b016
eposha <freedoctime@gmail.com>
2019-11-04 16:37:18 +0300

    5

--- JavaScript/lesson8__object/task2/index.js
+++ JavaScript/lesson8__object/task2/index.js
@@ -1,12 +1,13 @@
-const sortDesc = numbers =>
+const concatProps = obj => {
+    let arr = [];
 
-    // let [...sortedArray] = numbers;
-    // let sortedArray = numbers.slice();
-    numbers.slice().sort((a, b) => b - a);
-
-// return sortedArray.sort((a, b) =>
-//     b - a
-// );
-
-
-console.log(sortDesc([5, 9, 8, 7, 5, 55, 9, 66, 55, 95, 3, 65, 8, 66]));
+    for (let key in obj) {
+        arr.push(obj[key]);
+    }
+    return arr;
+}
+const object = {
+        name: 'Tom',
+        age: 17,
+    }
+    // console.log(concatProps(object));
