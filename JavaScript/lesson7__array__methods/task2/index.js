9431857b778d47b518ecb5792486ec6f7f84b354
eposha <freedoctime@gmail.com>
2019-11-04 11:55:58 +0300

    5

--- JavaScript/lesson7__array__methods/task2/index.js
+++ JavaScript/lesson7__array__methods/task2/index.js
@@ -1,6 +1,17 @@
 const sortDesc = numbers => {
-    const sortedArray = numbers
-        .sort();
+
+    let [...sortedArray] = numbers;
+
+    function comper(a, b) {
+        if (a < b) {
+            return +1;
+        } else {
+            return -1;
+        }
+    }
+    sortedArray.sort(comper);
 
     return sortedArray;
 };
+
+// console.log(sortDesc([5, 9, 8, 7, 5, 55, 9, 66, 55, 95, 3, 65, 8, 66]));
