7b5975f733a446ec348c21bed2687d400f969870
eposha <freedoctime@gmail.com>
2019-11-05 13:36:46 +0300

    5

--- JavaScript/lesson9__object/hw2/index.js
+++ JavaScript/lesson9__object/hw2/index.js
@@ -8,26 +8,27 @@ const getPeople = obj => {
     return arrFilter.map(i => i.map(i => i.name)).flat();
 }
 
-rooms = {
-    room1: [
-        { name: 'room1 name1' },
-        { name: 'room1 name2' },
-        { name: 'room1 name3' },
-        { name: 'room1 name4' },
+// rooms = {
+//     room1: [
+//         { name: 'room1 name1' },
+//         { name: 'room1 name2' },
+//         { name: 'room1 name3' },
+//         { name: 'room1 name4' },
 
-    ],
-    room2: [
-        { name: 'room2 name1' },
+//     ],
+//     room2: [
+//         { name: 'room2 name1' },
 
-    ],
-    room3: [
-        { name: 'room3 name1' },
-        { name: 'room3 name2' },
-        { name: 'room3 name3' },
+//     ],
+//     room3: [
+//         { name: 'room3 name1' },
+//         { name: 'room3 name2' },
+//         { name: 'room3 name3' },
 
-    ],
+//     ],
 
-};
+// };
 
 // console.log(getPeople(rooms));
 // console.log(rooms);
+// let big = 'toString';
