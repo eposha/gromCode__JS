e9b63725a2b57eb6d4edf50bbce7d024f4d6305e
eposha <freedoctime@gmail.com>
2019-11-05 13:34:06 +0300

    5

--- JavaScript/lesson9__object/hw2/index.js
+++ JavaScript/lesson9__object/hw2/index.js
@@ -1,9 +1,33 @@
-const getAdults = obj => {
-    let age = {};
-    for (let key in obj) {
-        if (obj[key] >= 18) {
-            age[key] = obj[key];
-        }
-    }
-    return age;
+const getPeople = obj => {
+
+    const arrEntries = Object.entries(obj);
+    const arrFlat = arrEntries.flat();
+    const arrFilter = arrFlat.filter(i => i.indexOf('room'));
+
+
+    return arrFilter.map(i => i.map(i => i.name)).flat();
 }
+
+rooms = {
+    room1: [
+        { name: 'room1 name1' },
+        { name: 'room1 name2' },
+        { name: 'room1 name3' },
+        { name: 'room1 name4' },
+
+    ],
+    room2: [
+        { name: 'room2 name1' },
+
+    ],
+    room3: [
+        { name: 'room3 name1' },
+        { name: 'room3 name2' },
+        { name: 'room3 name3' },
+
+    ],
+
+};
+
+// console.log(getPeople(rooms));
+// console.log(rooms);
