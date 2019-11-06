da6f0d579d9f35aba2c9a043ecabf8797bc06ef3
eposha <freedoctime@gmail.com>
2019-11-06 12:51:56 +0300

    5

--- JavaScript/lesson10__numbers/hw1/index.js
+++ JavaScript/lesson10__numbers/hw1/index.js
@@ -1,6 +1,13 @@
+// const getTotalPrice = arr => {
+
+//     return '$' + (arr.reduce((a, b) => a + b)).toFixed(2);
+
+// } --------------------- этот вариант не прошел, хотя результат тот же 
+
+
 const getTotalPrice = arr => {
 
-    return '$' + arr.reduce((a, b) => a + b).toFixed(2);
+    return '$' + Math.floor(arr.reduce((a, b) => a + b) * 100) / 100;
 
 }
 
