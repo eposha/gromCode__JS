220cf80e28536c1c62b61aa722235fa5a67dbd6f
eposha <freedoctime@gmail.com>
2019-11-06 13:18:59 +0300

    5

--- JavaScript/lesson10__numbers/hw1/index.js
+++ JavaScript/lesson10__numbers/hw1/index.js
@@ -1,18 +1,10 @@
 const getTotalPrice = arr => {
 
-        return '$' + +(arr.reduce((a, b) => a + b)).toFixed(2);
+    return '$' + Math.floor(arr.reduce((a, b) => a + b) * 100) / 100;
 
-    }
-    // --------------------- этот вариант не прошел, хотя результат тот же 
+}
 
-
-// const getTotalPrice = arr => {
-
-//     return '$' + Math.floor(arr.reduce((a, b) => a + b) * 100) / 100;
-
-// }
-
-const test = [5.5555, 9.3254, 87, -24, 5, -5];
+// const test = [5.5555, 9.3254, 87, -24, 5, -5];
 
 
-console.log(getTotalPrice(test));
+// console.log(getTotalPrice(test));
