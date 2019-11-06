10526a36ff6510f2f33b4d8133e3c073a46a3c9c
eposha <freedoctime@gmail.com>
2019-11-06 14:09:11 +0300

    5

--- JavaScript/lesson10__numbers/hw3/index.js
+++ JavaScript/lesson10__numbers/hw3/index.js
@@ -1,15 +1,15 @@
-const superRound = (num, fraction) => {
-
+const superRound = (num, frac) => {
+    const fraction = Math.pow(10, frac);
     return [
         Math.floor(num * fraction) / fraction,
         Math.round(num * fraction) / fraction,
         Math.ceil(num * fraction) / fraction,
-        Math.trunc(num * fraction) / fraction, +num.toFixed(fraction)
+        Math.trunc(num * fraction) / fraction, +num.toFixed(frac)
     ]
 
 }
 
-const testNum = -5.555;
-const testFraction = 1;
+// const testNum = -5.55566666666;
+// const testFraction = 20;
 
-console.log(superRound(testNum, testFraction));
+// console.log(superRound(testNum, testFraction));
