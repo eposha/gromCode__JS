33d27946ceeaf2928d234908ecebe4a071044271
eposha <freedoctime@gmail.com>
2019-11-06 13:08:52 +0300

    5

--- JavaScript/lesson10__numbers/hw2/index.js
+++ JavaScript/lesson10__numbers/hw2/index.js
@@ -1,32 +1,3 @@
-const getPeople = obj => {
-    const array = Object.values(obj)
-        .map(i => i.map(i => i.name));
+const getRandomNumbers = (a, b, c) => {
 
-    return array.concat.apply([], array);
 }
-
-
-
-// rooms = {
-//     room1: [
-//         { name: 'room1 name1' },
-//         { name: 'room1 name2' },
-//         { name: 'room1 name3' },
-//         { name: 'room1 name4' },
-
-//     ],
-//     room2: [
-//         { name: 'room2 name1' },
-
-//     ],
-//     room3: [
-//         { name: 'room3 name1' },
-//         { name: 'room3 name2' },
-//         { name: 'room3 name3' },
-
-//     ],
-
-// };
-
-// console.log(getPeople(rooms));
-// console.log(rooms);
--- JavaScript/lesson10__numbers/hw3/index.js
+++ JavaScript/lesson10__numbers/hw3/index.js
@@ -1,52 +1,16 @@
-const getCustomersList = obj => {
+const superRound = (num, fraction) => {
 
-    const copyObj = JSON.parse(JSON.stringify(obj));
-    const arrWithObj = Object.values(copyObj);
-    const objWithKey = Object.keys(copyObj);
+    return [
+        Math.floor(num * fraction) / fraction,
+        Math.round(num * fraction) / fraction,
+        Math.ceil(num * fraction) / fraction,
+        Math.trunc(num * fraction) / fraction,
+        num.toFixed(fraction)
+    ]
 
-
-    for (let i = 0; i < arrWithObj.length; i++) {
-        arrWithObj[i].id = objWithKey[i];
-    }
-
-
-    return arrWithObj.sort((a, b) => a.age > b.age ? 1 : -1);
 }
 
+// const testNum = 5.555;
+// const testFraction = 1;
 
-
-// const customers = {
-//     'customer-id-1': {
-//         name: 'William',
-//         age: 54
-//     },
-//     'customer-id-2': {
-//         name: 'Tom',
-//         age: 17
-//     },
-//     'customer-id-3': {
-//         name: 'Inna',
-//         age: 22
-//     },
-//     'customer-id-4': {
-//         name: 'Ivan',
-//         age: 18
-//     },
-// };
-
-// console.log(getCustomersList(customers));
-
-
-// console.log(customers);
-
-// const obj = {
-//     'Tom': 18,
-//     'Bob': 24,
-//     'Ivan': 17.
-// }
-
-// console.log(copyObj({
-//     'Tom': 18,
-//     'Bob': 24,
-//     'Ivan': 17.
-// }));
+// console.log(superRound(testNum, testFraction));
