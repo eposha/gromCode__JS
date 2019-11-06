d5e2ecf2d6f4ef8a2e6b55697d80a05b76131bda
eposha <freedoctime@gmail.com>
2019-11-06 14:39:42 +0300

    5

--- JavaScript/lesson10__numbers/hw1/index.js
+++ JavaScript/lesson10__numbers/hw1/index.js
@@ -1,12 +1,10 @@
-const getRandomNumbers = (length, start, end) => {
-    const startFixed = start.toFixed();
-    const endFixed = end.toFixed();
+const getTotalPrice = arr => {
 
-    let arrLength = Array(length);
-    return arrLength.slice(startFixed, endFixed);
+    return '$' + Math.floor(arr.reduce((a, b) => a + b) * 100) / 100;
+
 }
 
+// const test = [5.5555, 9.3254, 87, -24, 5, -5];
 
-// const test = [5,6,9,4,5.55,8,8.65];
 
-// console.log(getRandomNumbers(5, 1, 6));
+// console.log(getTotalPrice(test));
--- JavaScript/lesson10__numbers/hw2/index.js
+++ JavaScript/lesson10__numbers/hw2/index.js
@@ -1,3 +1,14 @@
-const getRandomNumbers = (a, b, c) => {
+const getRandomNumbers = (length, start, end) => {
+    const startFixed = Math.abs(+start.toFixed());
+    const endFixed = Math.abs(+end.toFixed());
+    console.log(startFixed);
+    console.log(endFixed);
 
+    let arrLength = Array(length);
+    return arrLength.slice(startFixed, endFixed);
 }
+
+
+// const test = [5,6,9,4,5.55,8,8.65];
+
+// console.log(getRandomNumbers(5, -1.25, -6,6));
