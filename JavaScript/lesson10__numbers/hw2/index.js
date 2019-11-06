86e58e30accb84f920c21d8d22f7c6dbafdae098
eposha <freedoctime@gmail.com>
2019-11-06 17:09:07 +0300

    5

--- JavaScript/lesson10__numbers/hw2/index.js
+++ JavaScript/lesson10__numbers/hw2/index.js
@@ -1,4 +1,12 @@
 const getRandomNumbers = (length, start, end) => {
+    if (start > end) return null;
+
+    if (function getRandomInt(start, end) {
+            start = Math.ceil(min);
+            end = Math.floor(max);
+            return Math.floor(Math.random() * (max - min)) + min;
+        });
+
 
     const finishArray = Array(length)
         .fill().map(i => Math.floor(Math.random() * (end - start + 1)) + start);
@@ -12,7 +20,7 @@ const getRandomNumbers = (length, start, end) => {
         }
     }
 
-    if (start > end && !checkInteg) {
+    if (!checkInteg) {
         return null;
     } else {
         return finishArray;
