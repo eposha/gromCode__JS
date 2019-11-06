2e514e99af751ec5b8adf233ac4863b738cec186
eposha <freedoctime@gmail.com>
2019-11-06 18:57:30 +0300

    5

--- JavaScript/lesson11__string/task1/index.js
+++ JavaScript/lesson11__string/task1/index.js
@@ -1,17 +1,18 @@
-const splitText = (text, len = 10) => {
-    const strArr = [];
-    let startPosition = 0;
-    if (typeof text !== 'string') return null;
-    while (true) {
-        let chunk = text.substr(startPosition, len);
-        if (chunk.length === 0) {
-            break;
-        }
-        strArr.push(chunk[0].toUpperCase() + chunk.slice(1));
-        startPosition += len;
+const splitText = (str, len = 10) => {
 
+    if (typeof str !== 'string') return null;
+
+    const arr = [];
+    let startPosition = 0;
+
+    while (true) {
+        let chunk = str.substr(startPosition, len);
+        if (chunk.length === 0) break;
+        arr.push(chunk[0].toUpperCase() + chunk.slice(1));
+        startPosition += len;
     }
-    return strArr.join('/n');
+
+    return arr.join('\n');
 }
 
 
