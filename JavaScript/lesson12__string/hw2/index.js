31f614c40c7ec6ee988070a4bb1628ee0cbafbcc
eposha <freedoctime@gmail.com>
2019-11-06 22:03:09 +0300

    5

--- JavaScript/lesson12__string/hw2/index.js
+++ JavaScript/lesson12__string/hw2/index.js
@@ -1,5 +1,5 @@
 const cleanTransactionsList = arr => {
-    return arr.filter(i => typeof + i === 'number').map(i => '$' + (+i).toFixed(2));
+    return arr.filter(i => typeof + i === 'number' && !isNaN(i)).map(i => '$' + (+i).toFixed(2));
 }
 
 // console.log(cleanTransactionsList([' 1.9 ', '16.4', 17, ' 1 dollar ']))
