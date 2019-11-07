ab57d7d3610ae7c73288452e0e6265c055b7f515
eposha <freedoctime@gmail.com>
2019-11-07 22:08:25 +0300

    4

--- JavaScript/lesson13/hw3/lesson9__object/__synopsis/index.js
+++
@@ -1,8 +0,0 @@
-//object
-const user = {
-    name: 'Tom',
-    age: 17,
-}
-Object.keys(user); // ['name', 'Tom']
-Object.values(user); // ['Tom', '17']
-Object.entries(user); // [['name', 'Tom'], ['age', '17']
