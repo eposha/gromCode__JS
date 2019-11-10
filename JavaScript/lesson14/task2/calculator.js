bc366d7d45d482058768624e156d2657985a1f7f
eposha <freedoctime@gmail.com>
2019-11-10 12:24:08 +0300

    5

--- JavaScript/lesson14/task2/calculator.js
+++
@@ -1,7 +0,0 @@
-export const getSquaredArray = arr =>
-    arr.map(i => i * i);
-
-export const getOddNumbers = arr =>
-    arr.filter(i => (i % 2 === 1));
-
-export default (a, b) => a + b;
---
+++ JavaScript/lesson14/task2/index.js
@@ -0,0 +1,21 @@
+export const createMessenger = (name, text, sender) => {
+    const sendMessage = name => {
+        let names = name;
+        return names;
+    };
+
+    const setMessage = text => {
+        let texts = text;
+        return texts;
+    };
+
+    const setSender = sender => {
+        let senders = sender;
+        return senders;
+    };
+
+    console.log(`${sendMessage(name)}, ${setMessage(text)} ${setSender(sender)}`)
+};
+
+
+createMessenger('Ann', 'Just learn it!', 'Your Gromcode');
