e04155de3933780993196d0d70911a6df6e8e5d1
eposha <freedoctime@gmail.com>
2019-11-11 13:58:26 +0300

    5

--- JavaScript/lesson15/hw2/index.js
+++ JavaScript/lesson15/hw2/index.js
@@ -34,7 +34,7 @@ export const createLogger = () => {
         if (type == undefined) {
             return result.sort((a, b) => b.dateTime - a.dateTime);
         } else {
-            return result.filter(i => i.type != type)
+            return result.filter(i => i.type == type)
         }
     }
 
@@ -48,8 +48,8 @@ export const createLogger = () => {
 
 
 // const log1 = createLogger();
-// console.log(log1);
+// // console.log(log1);
 // log1.warn('Are you Andrei?');
 // log1.error('Oops, we think that you are not Andrei');
 // log1.log('Sorry, we made a mistake, YOU are ANDREI!!!');
-// console.log(log1.getRecords());
+// console.log(log1.getRecords('warn'));
