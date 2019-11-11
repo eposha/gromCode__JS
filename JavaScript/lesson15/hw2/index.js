093c6ce837abb2777680b15fe58e74530dfe0862
eposha <freedoctime@gmail.com>
2019-11-11 13:51:59 +0300

    5

--- JavaScript/lesson15/hw2/index.js
+++ JavaScript/lesson15/hw2/index.js
@@ -5,29 +5,51 @@ const createLogger = () => {
     const warn = text => {
         result.push({
             message: text,
-            dateTime: newDate(),
-            type: text,
+            dateTime: new Date(),
+            type: 'warn',
 
         })
-    }
-    const warn = text => {
-        result.push({
-            message: text,
-            dateTime: newDate(),
-            type: text,
-
-        })
-    }
+    };
 
     const error = text => {
-        errorText = text;
-    }
+        result.push({
+            message: text,
+            dateTime: new Date(),
+            type: 'error',
+
+        })
+    };
 
     const log = text => {
-        logText = text;
+        result.push({
+            message: text,
+            dateTime: new Date(),
+            type: 'log',
+
+        })
+    };
+
+    const getRecords = (type) => {
+
+        if (type == undefined) {
+            return result.sort((a, b) => b.dateTime - a.dateTime);
+        } else {
+            return result.filter(i => i.type != type)
+        }
     }
 
-    const getRecords = () => {
-        // if ()
+    return {
+        warn,
+        error,
+        log,
+        getRecords,
     }
-}
+};
+
+
+// const log1 = createLogger();
+// console.log(log1);
+// log1.warn('Are you Andrei?');
+// log1.error('Oops, we think that you are not Andrei');
+// log1.log('Sorry, we made a mistake, YOU are ANDREI!!!');
+// console.log(log1.getRecords());
