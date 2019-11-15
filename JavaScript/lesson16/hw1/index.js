0b90a02c2fb7af95578607cfe67ae3f73ab6c416
eposha <freedoctime@gmail.com>
2019-11-15 14:14:51 +0300

    5

--- JavaScript/lesson16/hw1/index.js
+++ JavaScript/lesson16/hw1/index.js
@@ -1,32 +1,11 @@
-export const createCalculator = () => {
-    let count = 0;
+var a = 17;
 
-    return {
-        add: num => {
-            count += num;
-        },
+if (a) {
 
-        decrease: num => {
-            count -= num;
-        },
+    let a = 1;
 
-        reset: () => {
-            count = 0;
-        },
-        getMemo: () => {
-            return count;
-        },
-    }
+    console.log(a);
+
 }
 
-// let counter1 = createCalculator();
-// count1 = counter1.add(5);
-// count1 = counter1.decrease(1);
-// count1 = counter1.getDemo();
-// console.log(count1);
-
-// let counter2 = createCalculator();
-// count2 = counter2.add(8);
-// count2 = counter2.decrease(1);
-// count2 = counter2.getDemo();
-// console.log(count2);
+console.log(a);
