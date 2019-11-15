bc52ea85e040bc300ed13daefa55bc54f0470fd2
eposha <freedoctime@gmail.com>
2019-11-15 14:23:45 +0300

    5

--- JavaScript/lesson16/hw2/index.test.js
+++ JavaScript/lesson16/hw2/index.test.js
@@ -1,35 +1,35 @@
-import { createLogger } from './index';
+// import { createLogger } from './index';
 
-it('should be array', () => {
-    const log1 = createLogger();
+// it('should be array', () => {
+//     const log1 = createLogger();
 
-    log1.warn('Are you Andrei?');
-    log1.error('Oops, we think that you are not Andrei');
-    log1.log('Sorry, we made a mistake, YOU are ANDREI!!!');
+//     log1.warn('Are you Andrei?');
+//     log1.error('Oops, we think that you are not Andrei');
+//     log1.log('Sorry, we made a mistake, YOU are ANDREI!!!');
 
-    const types = Array.isArray(log1.getRecords());
+//     const types = Array.isArray(log1.getRecords());
 
-    expect(types).toEqual(true);
-});
+//     expect(types).toEqual(true);
+// });
 
-it('should be empty array', () => {
-    const log1 = createLogger();
+// it('should be empty array', () => {
+//     const log1 = createLogger();
 
-    log1.warn('Are you Andrei?');
-    log1.error('Oops, we think that you are not Andrei');
+//     log1.warn('Are you Andrei?');
+//     log1.error('Oops, we think that you are not Andrei');
 
-    const result = log1.getRecords('log');
+//     const result = log1.getRecords('log');
 
-    expect(result).toEqual([]);
-});
+//     expect(result).toEqual([]);
+// });
 
-it('should be array with type warn', () => {
-    const log1 = createLogger();
+// it('should be array with type warn', () => {
+//     const log1 = createLogger();
 
-    log1.warn('Are you Andrei?');
-    log1.error('Oops, we think that you are not Andrei');
+//     log1.warn('Are you Andrei?');
+//     log1.error('Oops, we think that you are not Andrei');
 
-    const result = log1.getRecords('warn')[0].type;
+//     const result = log1.getRecords('warn')[0].type;
 
-    expect(result).toEqual('warn');
-});
+//     expect(result).toEqual('warn');
+// });
