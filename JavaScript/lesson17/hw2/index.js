dbd98787bd5e93126506a40ab1b649a6b1711a5f
eposha <freedoctime@gmail.com>
2019-11-25 16:20:43 +0300

    5

--- JavaScript/lesson17/hw2/index.js
+++ JavaScript/lesson17/hw2/index.js
@@ -1,18 +1,21 @@
 const timer = {
     secondsPassed: 0,
     minsPassed: 0,
-    startTimer() {
-        if (this.secondsPassed == 60) {
-            this.secondsPassed = 0;
-            this.minsPassed += 1;
+    startTimer(check = true) {
+        if (check == true) {
+            if (this.secondsPassed == 60) {
+                this.secondsPassed = 0;
+                this.minsPassed += 1;
+            };
+            this.secondsPassed += 5;
+            console.log(this.secondsPassed);
+            setTimeout(() => {
+                this.startTimer();
+            }, 5000);
         };
-        this.secondsPassed += 5;
-        let forStop = setTimeout(() => {
-            this.startTimer();
-        }, 5000);
     },
     stopTimer() {
-        clearTimeout(this.startTimer)
+        this.startTimer(false)
     },
     getTime() {
         let checkLength = this.secondsPassed < 10 ? '0' + this.secondsPassed : this.secondsPassed;
@@ -25,6 +28,16 @@ const timer = {
 };
 
 
-// console.log(timer.getTime());
+// console.log(timer.startTimer());
+
+// setTimeout(() => {
+//     timer.stopTimer();
+// }, 10000);
+
+// setTimeout(() => {
+//     console.log(timer.getTime());
+// }, 20000);
+
+
 
 export { timer };
