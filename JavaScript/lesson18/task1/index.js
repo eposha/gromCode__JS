e74129d1cffd6937b8564037251b0c9072d445a7
eposha <freedoctime@gmail.com>
2019-11-25 21:55:32 +0300

    5

--- JavaScript/lesson18/task1/index.js
+++ JavaScript/lesson18/task1/index.js
@@ -5,8 +5,7 @@ const event = {
         { name: 'John', age: 18, email: 'example@server.com' }
     ],
     getInvitations() {
-        let visit = this.guests.filter(i => i.age >= 18);
-        return visit.map(i => {
+        return this.guests.filter(i => i.age >= 18).map(i => {
             return {
                 email: i.email,
                 message: `Dear ${i.name}! ${this.message}`,
