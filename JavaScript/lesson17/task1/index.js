ad49a0f0974c93b86bf6d52329dd69fc5aaf9b59
eposha <freedoctime@gmail.com>
2019-11-25 12:53:07 +0300

    check this

--- JavaScript/lesson17/task1/index.js
+++ JavaScript/lesson17/task1/index.js
@@ -2,7 +2,7 @@ const user = {
     firstName: 'John',
     lastName: 'Doe',
     getFullName() {
-        console.log(`${this.firstName} ${this.lastName}`);
+        return `${this.firstName} ${this.lastName}`;
     }
 }
 
