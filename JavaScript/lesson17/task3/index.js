844afcb14c3d25649b1e156bb53112ed3bae4141
eposha <freedoctime@gmail.com>
2019-11-25 13:06:59 +0300

    function wrapper for setTimeout and this

--- JavaScript/lesson17/task3/index.js
+++ JavaScript/lesson17/task3/index.js
@@ -48,17 +48,7 @@ function defer(func, ms) {
     return function() {
         setTimeout(() => func.call(this, ...arguments), ms);
     }
-}
+};
 
-const user = {
-    name: 'Tom',
-    sayHi() {
-        console.log(`Hi, I'm ${this.name}!`);
-    }
-}
 
-// sayHi();
-
-const deferredSayHi = defer(user.sayHi, 2000);
-
-deferredSayHi.call({ name: 'Bob' });
+export { defer };
