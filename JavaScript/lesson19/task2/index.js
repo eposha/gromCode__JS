63188ceb863667f197c7a1610c45c17c0b1bc829
eposha <freedoctime@gmail.com>
2019-11-26 16:27:36 +0300

    5

--- JavaScript/lesson19/task2/index.js
+++ JavaScript/lesson19/task2/index.js
@@ -1,14 +1,22 @@
-const wallet = {
-    transactions: [500, 700, 650, 499, 840],
-    getMax() {
-        return Math.max(...this.transactions);
-    },
-    getMin() {
-        return Math.min(...this.transactions);
-    },
+const getOwnProps = (obj) => {
+    const arrWithProps = [];
+    for (const prop in obj) {
+        if (obj.hasOwnProperty(prop) && typeof obj[prop] !== 'function') {
+            arrWithProps.push(prop);
+        }
+    }
+
+    return arrWithProps;
 };
 
+// const objj = {
+//     name: 'Ivan',
+//     surname: 'Petrov',
+//     age() {
+//         console.log(`15 years old`);
+//     },
+// };
 
-// console.log(wallet.getMax());
+// console.log(getOwnProps(objj));
 
-export { wallet };
+export { getOwnProps };
