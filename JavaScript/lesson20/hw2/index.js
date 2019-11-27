9978fd5a0cca2f938f4919778d5628f8bba6f81b
eposha <freedoctime@gmail.com>
2019-11-27 11:06:22 +0300

    5

--- JavaScript/lesson20/hw2/index.js
+++ JavaScript/lesson20/hw2/index.js
@@ -1,8 +1,8 @@
 class User {
     constructor(id, name, sessionId) {
-        this._id = `${id}`;
-        this._name = `${name}`;
-        this._sessionId = `${sessionId}`;
+        this.id = `${id}`;
+        this.name = `${name}`;
+        this.sessionId = `${sessionId}`;
     };
 };
 
