96ac2a2fa644360aa9405982459b73cb8a7ba4fd
eposha <freedoctime@gmail.com>
2019-11-28 22:54:39 +0300

    5

--- JavaScript/lesson22/hw1/index.js
+++ JavaScript/lesson22/hw1/index.js
@@ -10,37 +10,70 @@ const todos = [
 
 const renderListItems = listItems => {
     const listElem = document.querySelector('.list');
+    const btnElem = document.querySelector('.btn');
+    const inputElem = document.querySelector('.task-input');
 
-    const listItemsElems = listItems.map(({ text, done }) => {
+    const listItemsElems = listItems
+        .sort((a, b) => a.done - b.done).map(({ text, done }) => {
 
-        const listItemElem = document.createElement('li');
-        listItemElem.classList.add('list__item');
+            const listItemElem = document.createElement('li');
+            listItemElem.classList.add('list__item');
 
-        const checkboxItem = document.createElement('input');
-        checkboxItem.setAttribute('type', 'checkbox')
-        checkboxItem.checked = done;
-        if (done) {
-            listItemElem.classList.add('list__item_done');
-        }
-        checkboxItem.classList.add('list__item-checkbox');
+            const checkboxItem = document.createElement('input');
+            checkboxItem.setAttribute('type', 'checkbox');
+            checkboxItem.checked = done;
+            if (done) {
+                listItemElem.classList.add('list__item_done');
+            }
+            checkboxItem.classList.add('list__item-checkbox');
 
 
-        listItemElem.append(checkboxItem, text);
+            listItemElem.append(checkboxItem, text);
 
-        return listItemElem;
-    });
+            return listItemElem;
+        });
 
     listElem.append(...listItemsElems);
 
 
-    document.addEventListener('click', function(event) {
+    const checkDoneWork = event => {
         const checkElem = event.target;
+        console.log(checkElem);
+        if (checkElem.tagName != 'INPUT') return;
+
         if (checkElem.checked) {
             checkElem.parentNode.classList.add('list__item_done');
+            listElem.append(checkElem.parentNode);
         } else {
-            checkElem.parentNode.classList.remove('list__item_done')
+            checkElem.parentNode.classList.remove('list__item_done');
+            listElem.prepend(checkElem.parentNode);
         };
-    });
+    };
+    listElem.addEventListener('click', checkDoneWork);
+
+
+    const creatLiWithWork = () => {
+        const inputValue = inputElem.value;
+
+        if (inputValue == '') return;
+
+        const listItemElem = document.createElement('li');
+        listItemElem.classList.add('list__item');
+
+        // console.log(listItemElem.innerHTML = inputElem.value);
+        const checkboxItem = document.createElement('input');
+        checkboxItem.classList.add('list__item-checkbox');
+        checkboxItem.setAttribute('type', 'checkbox');
+
+
+        listItemElem.prepend(checkboxItem, inputValue);
+
+        listElem.prepend(listItemElem);
+
+        inputElem.value = '';
+    };
+
+    btnElem.addEventListener('click', creatLiWithWork);
 
 };
 
