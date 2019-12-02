23435a18df60b8b734b4333e0edaec5d60f65ce1
eposha <freedoctime@gmail.com>
2019-12-02 15:43:54 +0300

    5

--- JavaScript/lesson24/hw1/index.js
+++ JavaScript/lesson24/hw1/index.js
@@ -1,13 +1,13 @@
 const getDiff = (startDate, endDate) => {
-    const startDateDays = new Date(startDate).getDay();
-    const startDateHours = new Date(startDate).getHours();
-    const startDateMinutes = new Date(startDate).getMinutes();
-    const startDateSecond = new Date(startDate).getSeconds();
+    const startDateDays = new Date(startDate).getUTCDay();
+    const startDateHours = new Date(startDate).getUTCHours();
+    const startDateMinutes = new Date(startDate).getUTCMinutes();
+    const startDateSecond = new Date(startDate).getUTCSeconds();
 
-    const endDateDays = new Date(endDate).getDay();
-    const endDateHours = new Date(endDate).getHours();
-    const endDateMinutes = new Date(endDate).getMinutes();
-    const endDateSecond = new Date(endDate).getSeconds();
+    const endDateDays = new Date(endDate).getUTCDay();
+    const endDateHours = new Date(endDate).getUTCHours();
+    const endDateMinutes = new Date(endDate).getUTCMinutes();
+    const endDateSecond = new Date(endDate).getUTCSeconds();
 
     return `${Math.abs(startDateDays - endDateDays)}d ${Math.abs(startDateHours - endDateHours)}h ${Math.abs(startDateMinutes - endDateMinutes)}m ${Math.abs(startDateSecond - endDateSecond)}s`
 };
