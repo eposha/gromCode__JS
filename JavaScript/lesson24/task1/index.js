450733f1a763478278d135fd3a41c3b0d36de2af
eposha <freedoctime@gmail.com>
2019-12-02 15:11:50 +0300

    5

--- JavaScript/lesson24/task1/index.js
+++ JavaScript/lesson24/task1/index.js
@@ -1,10 +1,10 @@
-const weekDay = ['Mo', 'Tu', 'We', 'Th', 'Fr', 'St', 'Su'];
+const weekDay = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'St'];
 
 const dayOfWeek = (date, days) => {
     const day = new Date(date).getDate();
     const dateInFuture = new Date(date).setDate(day + days);
-
     return weekDay[new Date(dateInFuture).getDay()];
 };
+
 
 export { dayOfWeek };
