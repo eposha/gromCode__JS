e0b7c4e6a7594feea3dac1e8b1dbc9ae346c4168
eposha <freedoctime@gmail.com>
2019-12-15 00:00:43 +0300

    5

--- JavaScript/lesson27/hw3/index.js
+++ JavaScript/lesson27/hw3/index.js
@@ -1,16 +1,16 @@
-// export const sum = (value) => {
-//     let arr = [];
-//     let num = value;
-//     for (let i = 0; i < value; i++) {
-//         arr.push(num);
-//         num -= 1;
-//     };
-
-//     return arr.reduce((acc, next) => acc + next);
-// };
-
 export const sum = (value) => {
-    return value !== 1 ? value + sum(value - 1) : 1;
+    let arr = [];
+    let num = value;
+    for (let i = 0; i < value; i++) {
+        arr.push(num);
+        num -= 1;
+    };
+
+    return arr.reduce((acc, next) => acc + next);
 };
+
+// export const sum = (value) => {
+//     return value !== 1 ? value + sum(value - 1) : 1;
+// };
 
 // console.log(sum(4));
