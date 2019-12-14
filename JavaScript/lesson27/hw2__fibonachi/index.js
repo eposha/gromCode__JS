68430945400e9f9bb5174f8cd43e45cc05a48fe9
eposha <freedoctime@gmail.com>
2019-12-14 22:47:58 +0300

    fibonachi

---
+++ JavaScript/lesson27/hw2__fibonachi/index.js
@@ -0,0 +1,13 @@
+export const sum = (value) => {
+    let prev = 0,
+        next = 1;
+
+    for (let i = 0; i < value; i++) {
+        if (prev > value) {
+            return prev = next - prev;
+        } else {
+            next = prev + next;
+            prev = next - prev;
+        }
+    };
+};
