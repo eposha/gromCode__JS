975aa4ea641886971a2f3d1dc6261d2fec1a86b7
eposha <freedoctime@gmail.com>
2019-12-14 16:38:53 +0300

    compose

--- JavaScript/lesson27/task3__compose/index.js
+++ JavaScript/lesson27/task3__compose/index.js
@@ -1,16 +1,30 @@
 // compose(func1, func2, ...) => 
 
-const add3 = value => value + 3;
-const mult2 = value => value * 2;
-const div4 = value => value / 4;
+// const add3 = value => value + 3;
+// const mult2 = value => value * 2;
+// const div4 = value => value / 4;
 
-const compose = (...funcs) => value => {
+export const compose = (...funcs) => value => {
     return funcs.reduce((acc, func) => func(acc), value);
 };
 
-const doEverything = compose(
-    add3,
-    mult2,
-    div4,
+// const doEverything = compose(
+//     add3,
+//     mult2,
+//     div4,
 
-);
+// );
+
+// console.log(doEverything);
+
+
+// function withoutArrow(...funcs) {
+//     console.log(funcs);
+//     return function reduce(value) {
+//         return funcs.reduce((acc, func) => func(acc), value);
+//     }
+// };
+
+// withoutArrow(add3,
+//     mult2,
+//     div4, )
