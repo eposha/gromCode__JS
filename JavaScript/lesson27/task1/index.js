e511450475abfbd7e3ae2c3eb54ac96a5d0fa69e
eposha <freedoctime@gmail.com>
2019-12-14 15:45:26 +0300

    5

--- JavaScript/lesson27/task1/index.js
+++ JavaScript/lesson27/task1/index.js
@@ -27,6 +27,6 @@ const calc = initValue => {
     return calculator;
 };
 
-const result = calc(3).add(2).mult(4).div(10).substract(5).result();
+// const result = calc(3).add(2).mult(4).div(10).substract(5).result();
 
-console.log(result);
+// console.log(result);
