15c0f66a2a7d13f0bac5130afb8811329379534d
eposha <freedoctime@gmail.com>
2019-12-14 15:50:30 +0300

    5

--- JavaScript/lesson27/task2/index.js
+++ JavaScript/lesson27/task2/index.js
@@ -1,26 +1,4 @@
-const favorites = ['id-6', 'id-17'];
-
-const tree = {
-    id: 'id-1',
-    name: 'Products',
-    nodes: [{
-            id: 'id-2',
-            name: 'Food',
-            nodes: [{
-                id: 'id-6',
-                name: 'Drinks',
-                nodes: [],
-            }]
-        },
-        {
-            id: 'id-17',
-            name: 'Vehicles',
-            nodes: [],
-        }
-    ],
-};
-
-const markFavorites = (tree, favorites) => {
+export const markFavorites = (tree, favorites) => {
     const isFavorite = favorites.includes(tree.id);
 
     return {
@@ -29,5 +7,3 @@ const markFavorites = (tree, favorites) => {
         nodes: tree.nodes.map(childNode => markFavorites(childNode, favorites)),
     }
 };
-
-const result = markFavorites(tree, favorites);
