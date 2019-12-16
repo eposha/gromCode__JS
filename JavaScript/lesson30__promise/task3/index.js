548648efc763646e671641df4e82bb662f4b0056
eposha <freedoctime@gmail.com>
2019-12-16 22:56:51 +0300

    5

--- JavaScript/lesson30__promise/task3/index.js
+++ JavaScript/lesson30__promise/task3/index.js
@@ -1,19 +1,19 @@
 import { addImage } from './addImage.js';
 
 const addImageV2 = imgSrc => {
-    const resultPomise = addImage(imgSrc);
-    resultPomise.then((data) => {
+    // const resultPomise = addImage(imgSrc);
+    addImage(imgSrc).then((data) => {
         console.log(data);
     });
 
-    resultPomise.catch((data) => {
+    addImage(imgSrc).catch((data) => {
         console.log(data);
     });
 };
 
 // const imgSrc = 'https://i.5sfer.com/post/postImage/thumb-8ipwnn.jpg';
 
-// console.log(addImageV2(imgSrc));
+// addImageV2(imgSrc);
 // console.log(addImageV2(imgSrc));
 // console.log('Elka');
 
