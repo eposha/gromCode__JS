1b70176868d4403a16785689a34b13dacafc9ec8
eposha <freedoctime@gmail.com>
2019-12-16 18:26:55 +0300

    5

--- JavaScript/lesson29__asynchronous__code/hw2/index.js
+++ JavaScript/lesson29__asynchronous__code/hw2/index.js
@@ -1,17 +1,17 @@
 const requestUserData = (userId, callback) => {
-    if (userId == 'broken') {
-        callback(null, 'Failed to load user data');
-        return;
-    }
     let object = {
         name: 'John',
         age: 17,
         userId,
         email: 'userid777@example.com'
     };
-
-    callback = (object);
-    setTimeout(callback, Math.floor(Math.random() * 3 + 1) * 1000);
+    setTimeout(() => {
+        if (userId == 'broken') {
+            callback(null, 'Failed to load user data');
+            return;
+        }
+        callback(object);
+    }, Math.floor(Math.random() * 3 + 1) * 1000);
 }
 
 // requestUserData('broken');
