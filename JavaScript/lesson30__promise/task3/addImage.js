39e140e3ab07014d1e1f37403a5b2e80f2af0021
eposha <freedoctime@gmail.com>
2019-12-16 23:58:10 +0300

    5

--- JavaScript/lesson30__promise/task3/addImage.js
+++ JavaScript/lesson30__promise/task3/addImage.js
@@ -1,28 +1,37 @@
 const addImage = imgSrc => {
-    const p = new Promise((resolve, reject) => {
-        const imgElem = document.createElement('img');
-        imgElem.setAttribute('alt', 'My Photo');
-        imgElem.src = imgSrc;
-        const containerElem = document.querySelector('.page');
-        containerElem.append(imgElem);
-
-        const imgLoaded = () => {
-            const { width, height } = imgElem;
-            // resolve('good job');
-            resolve({ width, height });
-        };
-
-        const imgNotLoaded = () => {
-            reject('Image load failed');
-        };
-
-        imgElem.addEventListener('load', imgLoaded);
-        imgElem.addEventListener('error', imgNotLoaded);
-    });
-    return p;
+    const imgElem = document.createElement('img');
+    imgElem.setAttribute('alt', 'My Photo');
+    imgElem.src = imgSrc;
+    return imgElem;
 };
 
 export { addImage };
+
+// const addImage = imgSrc => {
+//     const p = new Promise((resolve, reject) => {
+//         const imgElem = document.createElement('img');
+//         imgElem.setAttribute('alt', 'My Photo');
+//         imgElem.src = imgSrc;
+//         const containerElem = document.querySelector('.page');
+//         containerElem.append(imgElem);
+
+//         const imgLoaded = () => {
+//             const { width, height } = imgElem;
+//             // resolve('good job');
+//             resolve({ width, height });
+//         };
+
+//         const imgNotLoaded = () => {
+//             reject('Image load failed');
+//         };
+
+//         imgElem.addEventListener('load', imgLoaded);
+//         imgElem.addEventListener('error', imgNotLoaded);
+//     });
+//     return p;
+// };
+
+// export { addImage };
 
 
 
--- JavaScript/lesson30__promise/task3/index.js
+++ JavaScript/lesson30__promise/task3/index.js
@@ -1,12 +1,27 @@
 import { addImage } from './addImage.js';
 
 const addImageV2 = imgSrc => {
-    return addImage(imgSrc);
+
+    return new Promise((resolve, reject) => {
+        const imageElem = addImage(imgSrc);
+        const containerElem = document.querySelector('.page');
+        containerElem.append(imageElem);
+
+        const imgLoaded = () => {
+            const { width, height } = imageElem;
+            resolve({ width, height });
+        };
+        const imgNotLoaded = () => {
+            reject('Image load failed');
+        };
+        imageElem.addEventListener('load', imgLoaded);
+        imageElem.addEventListener('error', imgNotLoaded);
+    });
 };
 
 
 
-// const imgSrc = 'https://i.5sfer.com/post/postImage/thumb-8ipwnn.jp';
+// const imgSrc = 'https://i.5sfer.com/post/postImage/thumb-8ipwnn.jpg';
 
 // addImageV2(imgSrc).catch(data => {
 //     console.log(data);
