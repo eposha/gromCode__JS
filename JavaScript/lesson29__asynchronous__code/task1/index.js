d587420b1e8418513792d0400aca92f301b96c5c
eposha <freedoctime@gmail.com>
2019-12-16 16:37:48 +0300

    5

--- JavaScript/lesson29__asynchronous__code/task1/index.js
+++ JavaScript/lesson29__asynchronous__code/task1/index.js
@@ -1,33 +1,3 @@
-// const addImage = (imgSrc, callback) => {
-//     const imgElem = document.createElement('img');
-//     imgElem.setAttribute('alt', 'My Photo');
-//     imgElem.src = imgSrc;
-//     const containerElem = document.querySelector('.page');
-//     containerElem.append(imgElem);
-
-//     const onImageLoaded = () => {
-//         const { width, height } = imgElem;
-//         callback(null, width, height);
-//     };
-
-//     imgElem.addEventListener('load', onImageLoaded);
-//     imgElem.addEventListener('error', () => callback('Image load failed'));
-
-// };
-
-// const onImageLoaded = (error, width, height) => {
-//     if (error) {
-//         // console.log(error);
-//         return;
-//     }
-//     const sizeElem = document.querySelector('.image-size');
-//     sizeElem.textContent = `${width} x ${height}`;
-// };
-
-// // addImage(imgSrc, onImageLoaded);
-// export { addImage };
-
-
 function addImage(imgSrc, callback) {
     const imgElem = document.createElement('img');
     imgElem.setAttribute('alt', 'My Photo');
@@ -55,25 +25,3 @@ function onImageLoaded(error, data) {
 };
 
 export { addImage };
-
-
-
-
-
-
-
-
-
-
-// const buttonElem = document.querySelector('#button');
-
-// buttonElem.addEventListener('click', () => console.log('Click'));
-
-// let sum = 0;
-// const startTime = new Date();
-// console.log('Cycle started');
-// for (let i = 0; i < 5e9; i++) {
-//     sum += i;
-// }
-// const endTime = new Date();
-// console.log(`Cycle took ${endTime - startTime}ms`);
