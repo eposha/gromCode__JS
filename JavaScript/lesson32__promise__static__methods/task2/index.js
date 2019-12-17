3e866d5f908f99320ad48d055ca08ff898483111
eposha <freedoctime@gmail.com>
2019-12-17 18:53:14 +0300

    5

--- JavaScript/lesson32__promise__static__methods/task2/index.js
+++ JavaScript/lesson32__promise__static__methods/task2/index.js
@@ -1,46 +1,10 @@
-// const servers = [
-//     'https://server.com.us',
-//     'https://server.com.eu',
-//     'https://server.com.au'
-// ];
-
-// const request = url => new Promise(resolve => {
-//     return new Promise((resolve) => {
-//         setTimeout(() => {
-//             resolve({
-//                 userData: {
-//                     name: 'Tom',
-//                     age: 17,
-//                 },
-//                 source: url,
-//             })
-//         }, (1000 + Math.random() * (3000 - 1000)));
-//     })
-// });
-
-// const getUserASAP = userId => {
-//     const userUrls = servers
-//         .map(serverUrl => `${serverUrl}/${userId}`);
-
-//     const requests = userUrls
-//         .map(userUrl => request(userUrl));
-
-//     return Promise.race(requests);
-// }
-
-// export { getUserASAP };
-
-
-
-
-
 const servers = [
-    'https://server.com/us/',
-    'https://server.com/eu/',
-    'https://server.com/au/'
+    'https://server.com.us',
+    'https://server.com.eu',
+    'https://server.com.au'
 ];
 
-function request(url) {
+const request = url => new Promise(resolve => {
     return new Promise((resolve) => {
         setTimeout(() => {
             resolve({
@@ -49,19 +13,20 @@ function request(url) {
                     age: 17,
                 },
                 source: url,
-            });
+            })
         }, (1000 + Math.random() * (3000 - 1000)));
-    });
-};
+    })
+});
 
-function getUserASAP(userId) {
-    const userUrls = servers.map(serverUrl => `${serverUrl}/${userId}`);
-    const requests = userUrls.map(userUrl => request(userUrl));
+const getUserASAP = userId => {
+    const userUrls = servers
+        .map(serverUrl => `${serverUrl}/${userId}`);
+
+    const requests = userUrls
+        .map(userUrl => request(userUrl));
 
     return Promise.race(requests);
-};
-
-// getUserASAP('7777').then(value => console.log(value));
+}
 
 export { getUserASAP };
 
