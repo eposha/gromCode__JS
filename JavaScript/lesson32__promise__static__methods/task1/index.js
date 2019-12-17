d66ec1bcf14328beb1d0e224408290eea0accd4d
eposha <freedoctime@gmail.com>
2019-12-17 18:18:23 +0300

    5

--- JavaScript/lesson32__promise__static__methods/task1/index.js
+++ JavaScript/lesson32__promise__static__methods/task1/index.js
@@ -1,25 +1,47 @@
-const detValueWithDelay = value => new Promise(resolve => resolve(value));
-
-const asyncNum1 = detValueWithDelay(56, 1000),
-    asyncNum2 = 'find',
-    asyncNum3 = detValueWithDelay(12, 2000);
-
-const getSum = numbers => numbers
-    .filter(value => !isNaN(value))
-    .reduce((acc, num) => acc + +num, 0);
-
-
-const asyncSum = (...asyncNumbers) => {
+function asyncSum(...asyncNumbers) {
     return Promise.all(asyncNumbers)
         .then(numbers => getSum(numbers))
-        .catch(() => Promise.reject(new Error(`Can't calculate`)))
+        .catch(() => Promise.reject(new Error('Can\'t calculate')));
 };
+
+function getSum(numbers) {
+    return numbers
+        .filter(elem => !isNaN(elem))
+        .reduce((acc, elem) => acc + Number(elem), 0);
+};
+
+export { asyncSum };
+
+
+
+
+// const detValueWithDelay = value => new Promise(resolve => resolve(value));
+
+// const asyncNum1 = detValueWithDelay(56, 1000),
+//     asyncNum2 = 'find',
+//     asyncNum3 = detValueWithDelay(12, 2000);
+
+// const getSum = numbers => numbers
+//     .filter(value => !isNaN(value))
+//     .reduce((acc, num) => acc + +num, 0);
+
+
+// const asyncSum = (...asyncNumbers) => {
+//     return Promise.all(asyncNumbers)
+//         .then(numbers => getSum(numbers))
+//         .catch(() => Promise.reject(new Error(`Can't calculate`)))
+// };
 
 // asyncSum(asyncNum1, Promise.reject(new Error('err')), asyncNum3)
 //     .then(result => console.log(result))
 //     .catch(result => console.log(result));
 
-export { asyncSum };
+// export { asyncSum };
+
+
+
+
+
 
 
 
