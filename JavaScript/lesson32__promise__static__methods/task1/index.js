3cba03e84c188f1e1db4ae2c76568ece31f11fe7
eposha <freedoctime@gmail.com>
2019-12-17 17:44:20 +0300

    5

---
+++ JavaScript/lesson32__promise__static__methods/task1/index.js
@@ -0,0 +1,81 @@
+const detValueWithDelay = value => new Promise(resolve => resolve(value));
+
+const getSum = numbers =>
+    numbers
+    .filter(value => !isNaN(value))
+    .reduce((acc, num) => acc + +num, 0);
+
+
+const asyncSum = (...asyncNumbers) => {
+    return Promise.all(asyncNumbers)
+        .then(numbers => getSum(numbers))
+        .catch(() => Promise.reject(new Error(`Can't calculate`)))
+};
+
+export { asyncSum };
+
+
+
+
+
+
+
+
+// const detValueWithDelay = (value, daley) => new Promise(resolve => {
+//     setTimeout(() => {
+//         console.log(value);
+//         resolve(value);
+//     }, daley);
+// });
+
+// const asyncNum1 = detValueWithDelay(56, 1000),
+//     asyncNum2 = detValueWithDelay(4, 2000),
+//     asyncNum3 = detValueWithDelay(12, 2000);
+
+// const getSum = numbers =>
+//     numbers
+//     .filter(value => !isNaN(value))
+//     .reduce((acc, num) => acc + +num, 0);
+
+
+// const asyncSum = (...asyncNumbers) => {
+//     return Promise.all(asyncNumbers)
+//         .then(numbers => getSum(numbers));
+// };
+
+// asyncSum(asyncNum1, asyncNum2, asyncNum3)
+//     .then(result => console.log(result));
+
+
+
+
+
+
+
+
+
+
+// const detValueWithDelay = (value, daley) => new Promise(resolve => {
+//     setTimeout(() => {
+//         console.log(value);
+//         resolve(value);
+//     }, daley);
+// });
+
+// const asyncNum1 = detValueWithDelay(56, 1000),
+//     asyncNum2 = detValueWithDelay(undefined, 2000),
+//     asyncNum3 = detValueWithDelay('10', 2000);
+
+// // const getSum = numbers =>
+// //     numbers
+// //     .filter(value => !isNaN(value))
+// //     .reduce((acc, num) => acc + +num, 0);
+
+
+// const asyncSum = (...asyncNumbers) => {
+//     return Promise.allSettled(asyncNumbers)
+//         .then(numbers => console.log(numbers));
+// };
+
+// asyncSum(asyncNum1, Promise.reject(new Error('err')), asyncNum3)
+//     .then(result => console.log(result));
---
+++ JavaScript/lesson32__promise__static__methods/task2/index.js
@@ -0,0 +1,37 @@
+const getRandomNumber = (from, to) =>
+    from + Math.random() * (to - from);
+
+
+const request = url => new Promise(resolve => {
+    const randomDelay = getRandomNumber(1000, 3000);
+
+    setTimeout(() => {
+        resolve({
+            userData: {
+                name: 'Tom',
+                age: 17,
+            },
+            source: url,
+        })
+    }, randomDelay);
+});
+
+
+const servers = [
+    'https://server.com.us',
+    'https://server.com.eu',
+    'https://server.com.au'
+]
+
+const getUserASAP = userId => {
+    const userUrls = servers
+        .map(serverUrl => `${serverUrl}/users/${userId}`);
+
+    const requests = userUrls
+        .map(userUrl => request(userUrl));
+
+    return Promise.race(requests);
+};
+
+getUserASAP('user-id-1')
+    .then(result => console.log(result));
