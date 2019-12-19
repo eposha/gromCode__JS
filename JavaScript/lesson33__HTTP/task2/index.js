a292c9d5bce2f36d161ee5f65745be616309a62d
eposha <freedoctime@gmail.com>
2019-12-19 21:13:23 +0300

    5

--- JavaScript/lesson33__HTTP/task2/index.js
+++ JavaScript/lesson33__HTTP/task2/index.js
@@ -20,6 +20,7 @@ const checkAvailable = (arr, data) => arr
 const countCommits = arr => arr
     .reduce((acc, nextValue) => {
         const check = checkAvailable(acc, nextValue.commit.author.email);
+
         if (check == undefined) {
             let obj = {};
             obj.count = 1;
@@ -40,7 +41,7 @@ const filterLessActiveDevs = arr => {
 
 
 const fetchMoreActivities = (startDate, userId, repoId) => {
-    return fetch(`https://api.github.com/repos/${userId}/${repoId}/commits`)
+    return fetch(`https://api.github.com/repos/${userId}/${repoId}/commits?per_page=100`)
         .then(commits => commits.json())
         .then(data => data.filter(obj => Date.parse(`${obj.commit.author.date}`) > startDate))
         .then(obj => countCommits(obj).splice(1))
