ca0eb8c430819e2f1b462fa77196b756da253ba4
eposha <freedoctime@gmail.com>
2019-12-19 20:46:19 +0300

    5

--- JavaScript/lesson33__HTTP/task2/index.js
+++ JavaScript/lesson33__HTTP/task2/index.js
@@ -1,34 +1,90 @@
-const servers = [
-    'https://server.com.us',
-    'https://server.com.eu',
-    'https://server.com.au'
-];
+const userAvatarElem = document.querySelector('.user__avatar');
+const userNameElem = document.querySelector('.user__name');
+const userLocationElem = document.querySelector('.user__location');
 
-const request = url => new Promise(resolve => {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            resolve({
-                userData: {
-                    name: 'Tom',
-                    age: 17,
-                },
-                source: url,
-            })
-        }, (1000 + Math.random() * (3000 - 1000)));
-    })
-});
+const defaultAvatar = 'https://avatars3.githubusercontent.com/u1000';
 
-const getUserASAP = userId => {
-    const userUrls = servers
-        .map(serverUrl => `${serverUrl}/${userId}`);
 
-    const requests = userUrls
-        .map(userUrl => request(userUrl));
+userAvatarElem.src = defaultAvatar;
 
-    return Promise.race(requests);
-}
+const showUserBtnElem = document.querySelector('.name-form__btn');
+const userNameInputeElem = document.querySelector('.name-form__input');
 
-export { getUserASAP };
 
-// getUserASAP('user-id-1')
-//     .then(result => console.log(result));
+
+const checkAvailable = (arr, data) => arr
+    .find(obj => {
+        return obj.email == data;
+    });
+
+const countCommits = arr => arr
+    .reduce((acc, nextValue) => {
+        const check = checkAvailable(acc, nextValue.commit.author.email);
+        if (check == undefined) {
+            let obj = {};
+            obj.count = 1;
+            obj.name = nextValue.commit.author.name;
+            obj.email = nextValue.commit.author.email;
+            acc.push(obj);
+        } else {
+            check.count++;
+        }
+        return acc;
+    }, [{}]);
+
+const filterLessActiveDevs = arr => {
+    arr.sort((a, b) => a.count > b.count);
+    const firstDev = arr[0].count;
+    return arr.filter(obj => obj.count == firstDev);
+};
+
+
+const fetchMoreActivities = (startDate, userId, repoId) => {
+    return fetch(`https://api.github.com/repos/${userId}/${repoId}/commits`)
+        .then(commits => commits.json())
+        .then(data => data.filter(obj => Date.parse(`${obj.commit.author.date}`) > startDate))
+        .then(obj => countCommits(obj).splice(1));
+};
+
+const getDateOfStartCalc = days => new Date().setDate(new Date().getDate() - days);
+
+const getMostActiveDevs = data => {
+    const { days, userId, repoId } = data;
+    const startDate = getDateOfStartCalc(days);
+    fetchMoreActivities(startDate, userId, repoId)
+        .then(data => filterLessActiveDevs(data));
+    // .then(data => filterLessActiveDevs(data));
+};
+
+export { getMostActiveDevs };
+
+// getMostActiveDevs({ days: 10, userId: 'eposha', repoId: 'project_calendar' })
+
+// showUserBtnElem.addEventListener('click', onSearchUser);
+
+
+
+
+
+
+// const fetchUserData = userName => {
+//     return fetch(`https://api.github.com/users/${userName}`)
+//         .then(response => response.json());
+// };
+
+// const renderUserData = userData => {
+//     const { avatar_url, name, location } = userData;
+//     userAvatarElem.src = avatar_url;
+//     userNameElem.textContent = name;
+//     userLocationElem.textContent = location ?
+//         `from ${location}` :
+//         '';
+// };
+
+// const onSearchUser = () => {
+//     const userName = userNameInputeElem.value
+//     fetchUserData(userName)
+//         .then(userData => renderUserData(userData));
+// };
+
+// showUserBtnElem.addEventListener('click', onSearchUser);
