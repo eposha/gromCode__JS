1db8113cef6d234efda1b10da1c51861b00e0739
eposha <freedoctime@gmail.com>
2019-12-23 12:06:59 +0300

    5

--- JavaScript/lesson34__HTTP2/hw1/src/js/postData.js
+++ JavaScript/lesson34__HTTP2/hw1/src/js/postData.js
@@ -8,19 +8,6 @@ const emailInputElem = document.querySelector('#email');
 const nameInputElem = document.querySelector('#name');
 const passwordInputElem = document.querySelector('#password');
 
-const checkStatus = data => {
-    if (data.status !== 201) {
-        errorElem.textContent = 'Failed to create user';
-    } else {
-        errorElem.textContent = '';
-        emailInputElem.value = '';
-        nameInputElem.value = '';
-        passwordInputElem.value = '';
-        submitBtnElem.disabled = true;
-        alert(data);
-    };
-};
-
 const postData = () => {
 
     const email = emailInputElem.value;
@@ -34,8 +21,17 @@ const postData = () => {
     };
 
     createUser(newUser)
-        .then(data => checkStatus(data));
-
+        .then(response => {
+            errorElem.textContent = '';
+            emailInputElem.value = '';
+            nameInputElem.value = '';
+            passwordInputElem.value = '';
+            submitBtnElem.disabled = true;
+            alert(response);
+        })
+        .catch(() => {
+            errorElem.textContent = 'Failed to create user';
+        });
 };
 
 const submitForm = () => {
--- JavaScript/lesson34__HTTP2/hw1/src/js/tasksGateway.js
+++ JavaScript/lesson34__HTTP2/hw1/src/js/tasksGateway.js
@@ -1,11 +1,12 @@
-const baseUrl = 'https://crudcrud.com/api/fcf76d7b403a4996956ac34a3bca29a9/tasks';
+const baseUrl = 'https://crudcrud.com/api/6698f1969127469c938ba2f1b54da39e/tasks';
 
 export const createUser = userData => {
     return fetch(baseUrl, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json;charset=utf-8',
-        },
-        body: JSON.stringify(userData)
-    })
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8',
+            },
+            body: JSON.stringify(userData)
+        }).then(response => response.json())
+        .then(data => JSON.stringify(data));
 };
