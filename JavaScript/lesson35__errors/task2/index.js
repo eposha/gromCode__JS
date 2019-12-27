3363ab770e95541a4ab38ef09ba3c9ef33fa1f46
eposha <freedoctime@gmail.com>
2019-12-27 23:02:30 +0300

    5

--- JavaScript/lesson35__errors/task2/index.js
+++ JavaScript/lesson35__errors/task2/index.js
@@ -1,48 +1,24 @@
-fetch(`https://api.github.com/user/github`)
-    .then(response => {
-        debugger;
-        if (response.status === 200) {
-            return response.json();
-        }
-        throw new Error('No data');
+const successRequest = Promise.resolve({ name: "Tom" });
+
+successRequest
+    .then(function onSuccess1(data) {
+        throw new Error('Error with data');
     })
-    .then(data => {
-        console.log(data);
-    })
-    .catch(err => {
-        debugger;
-        console.log(err)
+    .catch(function onError1(error) {
+        console.error("onError1", error.message);
     })
 
 
-// const successRequest = Promise.resolve({ name: 'Tom' });
+const failRequest = Promise.reject(new Error("Something went wrong"));
 
-
-// successRequest
-//     .then(data => {
-//         console.log(data);
-//         throw new Error('Unexpected error')
-//     })
-//     .catch(err => {
-//         console.warn(err.message);
-//     });
-
-
-// const failedRequest = Promise.reject(new Error('Fail'));
-
-// failedRequest
-//     .then(data => {
-//         console.log(data);
-//     })
-//     .catch(err => {
-//         console.log(err.message);
-//         throw err;
-//     })
-//     .then(data => {
-//         console.log(data);
-//     });
-
-
-// window.addEventListener('unhandledrejection', function(e) {
-//     console.log(e.reason.message)
-// });
+failRequest
+    .catch(function onError2(error) {
+        console.error("onError2", error.message);
+        throw new Error('Server error');
+    })
+    .then(function onSuccess2(data) {
+        console.log("onSuccess2", data);
+    })
+    .catch(function onError3(error) {
+        console.error("onError3", error.message);
+    });
