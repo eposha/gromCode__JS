ff61ed4f516d169d6c411d188f9ee925599b6fe1
eposha <freedoctime@gmail.com>
2019-12-30 14:59:17 +0300

    5

---
+++ JavaScript/lesson36__async_await/hw1/index.js
@@ -0,0 +1,33 @@
+const getUsersBlogs = async arr => {
+    const request = arr.map(userId => fetch(`https://api.github.com/users/${userId}`).then(response => {
+        if (response.ok) return response.json();
+        throw new Error('Failed to load data');
+    }));
+    const userData = await Promise.all(request);
+    return userData.map(user => user.blog);
+};
+
+getUsersBlogs(['google', 'facebook', 'gaearon'])
+    .then(data => console.log(data))
+
+
+
+
+// .then(linksList => console.log(linksList));
+// .then(emailList => console.log(emailList));
+//     .then(linksList => console.log(linksList));
+
+
+// const fetchUser = async userId => {
+//     try {
+//         const response = await fetch(`https://api.github.com/users/${userId}`);
+//         if (!response.ok) {
+//             return null;
+//         }
+//         const userData = await response.json();
+//         return userData
+//     } catch (err) {
+//         throw new Error('Failed to fetch user')
+//     }
+
+// };
--- JavaScript/lesson36__async_await/task1/index.js
+++ JavaScript/lesson36__async_await/task1/index.js
@@ -1,49 +1,35 @@
-export const fetchUser = async userId => {
+const getUser = async userId => {
     try {
         const response = await fetch(`https://api.github.com/users/${userId}`);
         if (!response.ok) {
             return null;
-        }
+        };
         const userData = await response.json();
-        return userData
+        return userData;
     } catch (err) {
-        throw new Error('Failed to fetch user')
+        throw new Error('Failed to fetch user');
     }
-
 };
 
-// console.log(fetchUser('google'))
+getUser('google')
+    .then(userData => console.log(userData))
+    .catch(err => console.log(err.message));
+
+// result
+//     .then(data => console.log(data))
 
 
-// const getUser = async userId => {
+
+// export const fetchUser = async userId => {
 //     try {
 //         const response = await fetch(`https://api.github.com/users/${userId}`);
 //         if (!response.ok) {
 //             return null;
 //         }
 //         const userData = await response.json();
-//         return userData;
+//         return userData
 //     } catch (err) {
-//         throw new Error('Failed to fetch user');
+//         throw new Error('Failed to fetch user')
 //     }
+
 // };
-
-// getUser('facebook')
-//     .then(userData => console.log(userData))
-//     .catch(err => alert('err.message'))
-
-
-// async function func() {
-//     const promise = new Promise(resolve => {
-//         setTimeout(() => {
-//             resolve(17);
-//         }, 1000);
-//     })
-//     const result = await promise;
-//     console.log(result);
-//     console.log('Hello');
-//     return 1;
-// };
-
-// const result = func();
-// console.log('Hi');
