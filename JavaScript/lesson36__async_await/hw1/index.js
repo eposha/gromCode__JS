6b4c65ed20a56434cfa329ce9f91cbedc9ec86d0
eposha <freedoctime@gmail.com>
2019-12-30 19:35:37 +0300

    5

--- JavaScript/lesson36__async_await/hw1/index.js
+++ JavaScript/lesson36__async_await/hw1/index.js
@@ -1,16 +1,13 @@
 export const getUsersBlogs = async arr => {
-    try {
-        let response = await arr.map(userId => fetch(`https://api.github.com/users/${userId}`)
-            .then(response => {
-                if (response.ok) return response.json()
-                throw new Error('Failed to load data');
-            })
-            .then(user => user.blog));
-        const usersData = await Promise.all(response);
-        return usersData;
-    } catch (err) {
-        console.log(err);
-    }
+    let response = await arr.map(userId => fetch(`https://api.github.com/users/${userId}`)
+        .then(response => {
+            if (response.ok) return response.json()
+            throw new Error('Failed to load data');
+        })
+        .then(user => user.blog)
+        .catch(err => console.log(err)));
+    const usersData = await Promise.all(response);
+    return usersData;
 
 };
 
