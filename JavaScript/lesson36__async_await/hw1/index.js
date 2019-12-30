cd3efb962e3acbbac7848143cc95a15feae0556c
eposha <freedoctime@gmail.com>
2019-12-30 15:04:37 +0300

    5

--- JavaScript/lesson36__async_await/hw1/index.js
+++ JavaScript/lesson36__async_await/hw1/index.js
@@ -1,33 +1,15 @@
-export const getUsersBlogs = async arr => {
-    const request = arr.map(userId => fetch(`https://api.github.com/users/${userId}`).then(response => {
-        if (response.ok) return response.json();
-        throw new Error('Failed to load data');
-    }));
-    const userData = await Promise.all(request);
-    return userData.map(user => user.blog);
-};
+  async function getUsersBlogs(usersList) {
+      const requests = usersList
+          .map(userId => fetch(`https://api.github.com/users/${userId}`)
+              .then(response => {
+                  if (response.ok) return response.json();
+                  throw new Error('Failed to load data');
+              }));
+      const usersData = await Promise.all(requests);
+      return usersData.map(user => user.blog);
+  };
 
-// getUsersBlogs(['google', 'facebook', 'gaearon'])
-//     .then(data => console.log(data))
+  // getUsersBlogs(['facebook', 'google', 'gaearon'])
+  //     .then(result => console.log(result));
 
-
-
-
-// .then(linksList => console.log(linksList));
-// .then(emailList => console.log(emailList));
-//     .then(linksList => console.log(linksList));
-
-
-// const fetchUser = async userId => {
-//     try {
-//         const response = await fetch(`https://api.github.com/users/${userId}`);
-//         if (!response.ok) {
-//             return null;
-//         }
-//         const userData = await response.json();
-//         return userData
-//     } catch (err) {
-//         throw new Error('Failed to fetch user')
-//     }
-
-// };
+  export { getUsersBlogs };
