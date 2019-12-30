9721cc0b1e3adf4613bce0f6a70b4be9d11c01cf
eposha <freedoctime@gmail.com>
2019-12-30 17:58:51 +0300

    5

--- JavaScript/lesson36__async_await/hw1/index.js
+++ JavaScript/lesson36__async_await/hw1/index.js
@@ -1,35 +1,41 @@
-export const getUsersBlogs = async arr => {
-    const request = arr.map(userId => fetch(`https://api.github.com/users/${userId}`)
+const getUsersBlogs = async arr => {
+    const request = await arr.map(userId => fetch(`https://api.github.com/users/${userId}`)
         .then(response => {
             if (response.ok) return response.json();
             throw new Error('Failed to load data');
-        })
-    );
-    const userData = await Promise.all(request);
-    return userData.map(user => user.blog);
+        }));
+    console.log(request)
+    let email = [];
+
+    const result = request
+        .map(arg => arg
+            .then(data => email.push(data.blog)))
+        // .then(response => response.map(arg => arg.json()))
+        // .then(data => data.map(arg => arg.then(data => email.push(data.blog))));
+        // return email;
+        // .then(data => console.log(data.map(arg => arg.blog))))
+        // .then(data => console.log(data))
+        // .then(response => response.blog);
+    return email;
+    // return result.map(data => data.blog);
+    // return request;
+    // const some = result.map(data => (data.json())
+    //     .then(data => data.blog));
+    // return some;
 };
 
 // getUsersBlogs(['google', 'facebook', 'gaearon'])
+//     .then(data => console.log(data))
 //     .then(data => console.log(data))
 
 
 
 
-// .then(linksList => console.log(linksList));
-// .then(emailList => console.log(emailList));
-//     .then(linksList => console.log(linksList));
-
-
-// const fetchUser = async userId => {
-//     try {
-//         const response = await fetch(`https://api.github.com/users/${userId}`);
-//         if (!response.ok) {
-//             return null;
-//         }
-//         const userData = await response.json();
-//         return userData
-//     } catch (err) {
-//         throw new Error('Failed to fetch user')
-//     }
-
+// async function showAvatar() {
+//     let response = await fetch(`https://api.github.com/users/google`);
+//     let user = await response.json();
+//     return user.blog;
 // };
+
+// showAvatar()
+//     .then(user => console.log(user));
